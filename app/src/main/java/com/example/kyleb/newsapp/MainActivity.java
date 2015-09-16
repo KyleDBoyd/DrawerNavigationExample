b4f329b4a929e51af364cced88dc2787d8c1fb51
Kyle Boyd <kyleboyd@live.ca>
2015-09-16 09:30:18 -0700

    Initial commit. Setup Android Annotations

---
+++ app/src/main/java/com/example/kyleb/newsapp/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.kyleb.newsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/kyleb/newsapp/MainApplication.java
@@ -0,0 +1,9 @@
+package com.example.kyleb.newsapp;
+
+import android.app.Application;
+
+/**
+ * Created by kyleb on 2015-09-16.
+ */
+public class MainApplication extends Application {
+}
